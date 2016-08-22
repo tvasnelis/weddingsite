92a1df0892e670fa5abd3c55c235c82dd5618c0b
tvasnelis <tim.vasnelis@gmail.com>
2016-08-22 17:47:18 -0400

    changed stay stlyes (option 2) WIP

--- stay.php
+++ stay.php
@@ -1,8 +1,54 @@
 <div id="stay" class="container-fluid text-center sub-font" href="#stay">
   <div class="container-fluid">
     <div class="row">
+      <div class="stay-header">
+          <h3 class="">Sleep It Off</h3>
+      </div>
+    </div>
+    <div class="row stay-location sl_first">
+      <div class="col-xs-7 col-sm-4 stay-img">
+        <a href="http://www.hotelmonteleone.com/" target="_blank"><img src="images/montelone.jpg" alt="Hotel Monteleone"></a>
+      </div>
+      <div class="col-xs-5 col-sm-4 stay-location">
+        <div class="align-left sm-address">
+          <h4><a href="http://www.hotelmonteleone.com/" target="_blank">Hotel Monteleone</a></h4>
+          <p>214 Royal St.</p>
+        </div>
+        <div class="xs-hide align-right sm-address">
+          <h4><a href="http://bienvillehouse.com/" target="_blank">Bienville House</a></h4>
+          <p>320 Decatur St.</p>
+        </div>
+      </div>
+      <div class="col-sm-4 stay-img xs-hide">
+        <a href="http://bienvillehouse.com/" target="_blank"><img src="images/bienville.jpg" alt="Bienville House"></a> 
+      </div>
 
-      <div class="col-sm-4">
+
+    </div>
+    <div class="row stay-location sm-hide">
+      <div class="col-xs-5 stay-location ">
+        <h4 class="align-right"><a href="http://bienvillehouse.com/" target="_blank">Bienville House</a></h4>
+        <p class="align-right">320 Decatur St.</p>
+      </div>
+      <div class="col-xs-7 stay-img">
+        <a href="http://bienvillehouse.com/" target="_blank"><img src="images/bienville.jpg" alt="Bienville House"></a> 
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <!-- <div class="col-sm-4">
         <a href="http://www.hotelmonteleone.com/" target="_blank"><img src="images/montelone.jpg" alt="Hotel Monteleone"></a>
       </div>
 
@@ -45,7 +91,7 @@
 
       <div class="col-sm-4">
         <a href="http://bienvillehouse.com/" target="_blank"><img src="images/bienville.jpg" alt="Bienville House"></a>
-      </div>
+      </div> -->
 
     </div>
   </div>
