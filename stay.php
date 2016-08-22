0a9dad808abe65f5b3a64b85cf2f03c32ef2b2e3
trinity-tim <tvasnelis@trinitymfgcorp.com>
2016-08-22 07:55:57 -0400

    stay panel styling, wedding guests

--- stay.php
+++ stay.php
@@ -6,41 +6,35 @@
         <a href="http://www.hotelmonteleone.com/" target="_blank"><img src="images/montelone.jpg" alt="Hotel Monteleone"></a>
       </div>
 
-      <div class="col-sm-4 stay-header">
+      <div class="col-sm-4">
         <div class="container-fluid">
           <div class="row text-center">
 
-            <div class="col-xs-4">
+            <div class="col-xs-4 stay-header">
               <h3 class="">Sleep It Off</h3>
             </div>
 
             <div class="col-xs-8 bg-red">
-              <div class="container-fluid">
                 <div class="row text-center">
-                  <div class="stay-location">
-                    <div class="col-xs-6 bg-yellow">
-                      <span class="glyphicon glyphicon-arrow-left"></span>
-                      <span class="glyphicon glyphicon-arrow-up"></span>
-                    </div>
-                    <div class="col-xs-6 bg-mediumgrey">
-                      <h4><a href="http://www.hotelmonteleone.com/" target="_blank">Hotel Monteleone</a></h4>
-                      <p>214 Royal St.</p>
-                    </div>
-                  </div> 
-                </div>
-                <div class="row text-center">
-                  <div class="stay-location">
-                    <div class="col-xs-6">
-                      <h4><a href="http://bienvillehouse.com/" target="_blank">Bienville House</a></h4>
-                      <p>320 Decatur St.</p>
-                    </div>
-                    <div class="col-xs-6">
-                      <span class="glyphicon glyphicon-arrow-right"></span>
-                      <span class="glyphicon glyphicon-arrow-down"></span>
-                    </div>
+                  <div class="col-xs-6 bg-yellow">
+                    <span class="glyphicon glyphicon-arrow-left stay-icon"></span>
+                    <span class="glyphicon glyphicon-arrow-up stay-icon"></span>
+                  </div>
+                  <div class="col-xs-6 bg-mediumgrey">
+                    <h4><a href="http://www.hotelmonteleone.com/" target="_blank">Hotel Monteleone</a></h4>
+                    <p>214 Royal St.</p>
                   </div>
                 </div>
-              
+                <div class="row text-center">
+                  <div class="col-xs-6 bg-mediumgrey">
+                    <h4><a href="http://bienvillehouse.com/" target="_blank">Bienville House</a></h4>
+                    <p>320 Decatur St.</p>
+                  </div>
+                  <div class="col-xs-6 stay-icon bg-yellow">
+                    <span class="glyphicon glyphicon-arrow-right"></span>
+                    <span class="glyphicon glyphicon-arrow-down"></span>
+                  </div>
+              </div>
             </div>
           </div>
         </div>
@@ -52,5 +46,5 @@
       </div>
 
     </div>
-  </div> 
+  </div>
 </div>
