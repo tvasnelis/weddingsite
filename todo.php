2367295a0e8b2400d8a3b69fdf5c0afca6601f89
tvasnelis <tim.vasnelis@gmail.com>
2017-02-16 16:08:08 -0500

    updated todo panel layout and text

--- todo.php
+++ todo.php
@@ -1,32 +1,36 @@
 <div id="todo" class="container-fluid text-center sub-font bg-green white-text" href="#todo">
   <div class="col-sm-2 todo-nav">
     <div class="row text-center todo-nav-item">
-      <label for="radio_drink">Drink Up</label>
       <input type="radio" name="todo" id="radio_drink" value="drink">
+      <label for="radio_drink">Drink Up</label>
     </div>
     <div class="row text-center todo-nav-item">
-      <label for="radio_eat">Eat</label>
       <input type="radio" name="todo" id="radio_eat" value="eat">
+      <label for="radio_eat">Eat</label>
     </div>
     <div class="row text-center todo-nav-item">
-      <label for="radio_explore">Explore</label>
       <input type="radio" name="todo" id="radio_explore" value="explore">
+      <label for="radio_explore">Explore</label>
     </div>
     <div class="row text-center todo-nav-item">
-      <label for="radio_tourist">Be a Tourist</label>
       <input type="radio" name="todo" id="radio_tourist" value="tourist">
+      <label for="radio_tourist">Be a Tourist</label>
     </div>
-    <div class="row text-center todo-nav-close">
-      <label for="radio_landing">Close</label>
-      <input type="radio" name="todo" id="radio_landing" value="landing">
-    </div>
+  </div>
+
+  <div class="todo-nav-close"> 
+    <input type="radio" name="todo" id="radio_landing" value="landing">
+    <label for="radio_landing">&#10006</label>
   </div>
 
   <div class="col-sm-10 todo-section">
     <div id="landing" class="todo-landing">
       <h3>Have Some Fun</h3>
-      <p>There is a lot to love about New Orleans and we are excited that for many of our guests it will be their first time visiting.  </p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div class="landing-text">
+        <p>There is a lot that we love about New Orleans.  The music, history, art, architecture, and southern hospitality make it a truly unique place that is easily accessible and inviting.  But for us, what's really important is the amazing food and drinks.</p>
+        <p>We are excited that for many of our guests it will be their first time visiting.  We hope you are able to get out and explore the city and find something you love.  We look forward to sharing this experience with all of you!</p>
+        <p>Explore some of our favorite places using the menu on the left or checkout the <a href="map_page" target="_blank">map.</a></p>
+      </div>
     </div>
     <div id="drink" class="todo-detail">
         <div id="carousel_drink" class="carousel slide" data-ride="carousel" data-interval="false">
@@ -36,29 +40,74 @@
 
             <div class="item active">
               <h4><a href="http://hotelmonteleone.com/entertainment/carousel-bar/" target="_blank">Carousel Bar</a></h4>
-              <p>French Quarter</p>
-              <p><a href="https://www.google.com/maps/place/Carousel+Bar/@29.954163,-90.068029,15z/data=!4m5!3m4!1s0x0:0xdd909df48675434d!8m2!3d29.954163!4d-90.068029" target="_blank">214 Royal Street</a></p>
+              <div class="item-address">
+                <p><a href="https://www.google.com/maps/place/Carousel+Bar/@29.954163,-90.068029,15z/data=!4m5!3m4!1s0x0:0xdd909df48675434d!8m2!3d29.954163!4d-90.068029" target="_blank">214 Royal Street</a></p>
+                <p>French Quarter</p>
+              </div>
+              <div class="item-text">
+                <p>The iconic Carousel Bar is located inside the Hotel Monteleone and overlooks Royal Street in the French Quarter.  The circular bar, which slowly rotates, is a wonderful place to kill and hour or two enjoying your favorite cocktail and mingling with the other riders.</p>
+              </div>
+              <p>Signature Cocktail: Vieux Carre</p>
+              <p>Tim's Drink: Bourbon Milk Punch</p>
+              <p>Kimberly's Drink: Aviation</p>
             </div>
+
             <div class="item">
               <h4><a href="https://www.arnaudsrestaurant.com/bars/french-75/" target="_blank">Arnaud's French 75</a></h4>
-              <p>French Quarter</p>
-              <p><a href="https://www.google.com/maps/place/French+75/@29.9557933,-90.0686926,15z/data=!4m5!3m4!1s0x0:0xbbb24fa3f0588a4c!8m2!3d29.9557933!4d-90.0686926"target="_blank">813 Bienville Street</a></p>
+              <div class="item-address">
+                <p><a href="https://www.google.com/maps/place/French+75/@29.9557933,-90.0686926,15z/data=!4m5!3m4!1s0x0:0xbbb24fa3f0588a4c!8m2!3d29.9557933!4d-90.0686926"target="_blank">813 Bienville Street</a></p>
+                <p>French Quarter</p>
+              </div>
+              <div class="item-text">
+                <p>In a city famous for the availability of alcohol in excess, what often gets forgotten is the historical significance of New Orleans and cocktails.  This place feels like a time warp to an era we never experienced first hand.</p>
+              </div>
+              <p>Signature Cocktail: French 75</p>
+              <p>Tim's Drink: Creole</p>
+              <p>Kimberly's Drink: French 75</p>
             </div>
+
             <div class="item">
               <h4><a href="http://curenola.com/" target="_blank">Cure</a></h4>
-              <p>Uptown</p>
-              <p><a href="https://www.google.com/maps/place/Cure/@29.9350286,-90.1096536,17z/data=!3m1!4b1!4m5!3m4!1s0x8620a5a9359e0abb:0x971b22a78c122688!8m2!3d29.935024!4d-90.1074596" target="_blank">4905 Freret Street</a></p>
+              <div class="item-address">
+                <p><a href="https://www.google.com/maps/place/Cure/@29.9350286,-90.1096536,17z/data=!3m1!4b1!4m5!3m4!1s0x8620a5a9359e0abb:0x971b22a78c122688!8m2!3d29.935024!4d-90.1074596" target="_blank">4905 Freret Street</a></p>
+                <p>Uptown</p>
+              </div>
+              <div class="item-text">
+                <p>###</p>
+              </div>
+              <p>###</p>
+              <p>###</p>
+              <p>###</p>
             </div>
+
             <div class="item">
               <h4><a href="http://hottinbar.com/" target="_blank">Hot Tin</a></h4>
-              <p>Garden District</p>
-              <p><a href="https://www.google.com/maps/place/Hot+Tin/@29.9351131,-90.0797176,15z/data=!4m2!3m1!1s0x0:0xca107f3e51a95c55?sa=X&ved=0ahUKEwju__qSuovSAhVBPCYKHWz9BpIQ_BIIhgEwCg" target="_blank">2031 Saint Charles Avenue</a></p>
+              <div class="item-address">
+                <p><a href="https://www.google.com/maps/place/Hot+Tin/@29.9351131,-90.0797176,15z/data=!4m2!3m1!1s0x0:0xca107f3e51a95c55?sa=X&ved=0ahUKEwju__qSuovSAhVBPCYKHWz9BpIQ_BIIhgEwCg" target="_blank">2031 Saint Charles Avenue</a></p>
+                <p>Garden District</p>
+              </div>
+              <div class="item-text">
+                <p>###</p>
+              </div>
+              <p>###</p>
+              <p>###</p>
+              <p>###</p>
             </div>
+
             <div class="item">
               <h4><a href="http://www.erinrosebar.com/" target="_blank">Erin Rose</a></h4>
-              <p>French Quarter</p>
-              <p><a href="https://www.google.com/maps/place/Erin+Rose/@29.9565322,-90.0701209,17z/data=!3m1!4b1!4m5!3m4!1s0x8620a60c2a3b32d3:0x45ea80baa89d884b!8m2!3d29.9565276!4d-90.0679269" target="_blank">811 Conti Street</a></p>
+              <div class="item-address">
+                <p><a href="https://www.google.com/maps/place/Erin+Rose/@29.9565322,-90.0701209,17z/data=!3m1!4b1!4m5!3m4!1s0x8620a60c2a3b32d3:0x45ea80baa89d884b!8m2!3d29.9565276!4d-90.0679269" target="_blank">811 Conti Street</a></p>
+                <p>French Quarter</p>
+              </div>
+              <div class="item-text">
+                <p>###</p>
+              </div>
+              <p>###</p>
+              <p>###</p>
+              <p>###</p>
             </div>
+
           </div>
           <div class="carousel-controls">  
             <!-- Left and right controls -->
@@ -84,57 +133,100 @@
     <div id="eat" class="todo-detail">
       <div id="carousel_eat" class="carousel slide" data-ride="carousel" data-interval="false">
 
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner" role="listbox">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
 
-            <div class="item active">
-              <h4><a href="https://www.sylvainnola.com/" target="_blank">Sylvain</a></h4>
+          <div class="item active">
+            <h4><a href="https://www.sylvainnola.com/" target="_blank">Sylvain</a></h4>
+            <div class="item-address">
+              <p><a href="#" target="_blank">912 Royal Street</a></p> 
               <p>French Quarter</p>
-              <p><a href="#" target="_blank">912 Royal Street</a></p>
             </div>
-            <div class="item">
-              <h4><a href="http://cochonrestaurant.com/" target="_blank">Cochon/Butcher</a></h4>
-              <p>Warehouse District</p>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
+          </div>
+
+          <div class="item">
+            <h4><a href="http://cochonrestaurant.com/" target="_blank">Cochon/Butcher</a></h4>
+            <div class="item-address">
               <p><a href="#" target="_blank">930 Tchoupitoulas Street</a></p>
-            </div>
-            <div class="item">
-              <h4><a href="http://coquettenola.com/" target="_blank">Coquette</a></h4>
-              <p>Garden District</p>
-              <p><a href="#" target="_blank">2800 Magazine Street</a></p>
-            </div>
-            <div class="item">
-              <h4><a href="http://www.pizzadomenica.com/" target="_blank">Pizza Domenica</a></h4>
-              <p>Uptown</p>
-              <p><a href="https://www.google.com/maps/place/PIZZA+domenica/@29.9204938,-90.1088353,15z/data=!4m5!3m4!1s0x0:0xea94fab39a45b76d!8m2!3d29.9204938!4d-90.1088353" target="_blank">4933 Magazine Street</a></p>
-            </div>
-            <div class="item">
-              <h4><a href="http://www.seaworthynola.com/" target="_blank">Seaworthy</a></h4>
               <p>Warehouse District</p>
-              <p><a href="https://www.google.com/maps/place/SEAWORTHY/@29.947879,-90.072208,15z/data=!4m5!3m4!1s0x0:0x1a15023dd6c1e365!8m2!3d29.947879!4d-90.072208" target="_blank">630 Carondelet Street</a></p>
             </div>
-            
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
           </div>
-          <div class="carousel-controls">  
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#carousel_eat" data-slide="prev">
-              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-              <li data-target="#carousel_eat" data-slide-to="0" class="active"></li>
-              <li data-target="#carousel_eat" data-slide-to="1"></li>
-              <li data-target="#carousel_eat" data-slide-to="2"></li>
-              <li data-target="#carousel_eat" data-slide-to="3"></li>
-              <li data-target="#carousel_eat" data-slide-to="4"></li>
-            </ol>
-            <a class="right carousel-control" href="#carousel_eat" data-slide="next">
-              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
+
+          <div class="item">
+            <h4><a href="http://coquettenola.com/" target="_blank">Coquette</a></h4>
+            <div class="item-address">
+              <p><a href="#" target="_blank">2800 Magazine Street</a></p>
+              <p>Garden District</p>
+            </div>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
           </div>
+          <div class="item">
+            <h4><a href="http://www.pizzadomenica.com/" target="_blank">Pizza Domenica</a></h4>
+            <div class="item-address">
+              <p><a href="https://www.google.com/maps/place/PIZZA+domenica/@29.9204938,-90.1088353,15z/data=!4m5!3m4!1s0x0:0xea94fab39a45b76d!8m2!3d29.9204938!4d-90.1088353" target="_blank">4933 Magazine Street</a></p>
+              <p>Uptown</p>
+            </div>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
+          </div>
+
+          <div class="item">
+            <h4><a href="http://www.seaworthynola.com/" target="_blank">Seaworthy</a></h4>
+            <div class="item-address">
+              <p><a href="https://www.google.com/maps/place/SEAWORTHY/@29.947879,-90.072208,15z/data=!4m5!3m4!1s0x0:0x1a15023dd6c1e365!8m2!3d29.947879!4d-90.072208" target="_blank">630 Carondelet Street</a></p>
+              <p>Warehouse District</p>
+            </div>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
+          </div>
+
+        </div>
+        <div class="carousel-controls">  
+          <!-- Left and right controls -->
+          <a class="left carousel-control" href="#carousel_eat" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carousel_eat" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel_eat" data-slide-to="1"></li>
+            <li data-target="#carousel_eat" data-slide-to="2"></li>
+            <li data-target="#carousel_eat" data-slide-to="3"></li>
+            <li data-target="#carousel_eat" data-slide-to="4"></li>
+          </ol>
+          <a class="right carousel-control" href="#carousel_eat" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
       </div>
+    </div>
 
     <div id="explore" class="todo-detail">
       <div id="carousel_explore" class="carousel slide" data-ride="carousel" data-interval="false">
@@ -144,19 +236,43 @@
 
           <div class="item active">
             <h4><a href="https://preservationhall.com/" target="_blank">Preservation Hall</a></h4>
-            <p>French Quarter</p>
-            <p><a href="#" target="_blank">726 Saint Peter Steet</a></p>
-          </div>
+            <div class="item-address">
+              <p><a href="#" target="_blank">726 Saint Peter Steet</a></p>
+              <p>French Quarter</p>
+            </div>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
+          </div>  
           <div class="item">
             <h4><a href="https://www.spottedcatmusicclub.com/" target="_blank">The Spotted Cat</a></h4>
-            <p>Frenchman Street</p>
-            <p><a href="#" target="_blank">623 Frenchmen Street</a></p>
-          </div>
+            <div class="item-address">
+              <p><a href="#" target="_blank">623 Frenchmen Street</a></p> 
+              <p>Frenchman Street</p>
+            </div>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
+          </div>  
           <div class="item">
             <h4><a href="http://www.nationalww2museum.org/" target="_blank">National WWII Museum</a></h4>
-            <p>Central Business District</p>
-            <p><a href="#" target="_blank">925 Magazine Street</a></p>
-          </div>
+            <div class="item-address">
+              <p><a href="#" target="_blank">925 Magazine Street</a></p>
+              <p>Central Business District</p>
+            </div>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
+          </div>    
         </div>
         <div class="carousel-controls">  
           <!-- Left and right controls -->
@@ -184,16 +300,45 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
 
-          <div class="item active">
+           <div class="item">
             <h4><a href="http://www.cafedumonde.com/" target="_blank">Cafe Du Monde</a></h4>
-            <p>French Quarter</p>
-            <p><a href="#" target="_blank">800 Decatur Street</a></p>
-          </div>
-          <div class="item">
+            <div class="item-address">
+              <p><a href="#" target="_blank">925 Magazine Street</a></p>
+              <p>French Quarter</p>
+            </div>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
+          </div> 
+           <div class="item">
             <h4><a href="http://www.acmeoyster.com/" target="_blank">Acme Oyster House</a></h4>
-            <p>French Quarter</p>
-            <p><a href="#" target="_blank">800 Decatur Street</a></p>
-          </div>
+            <div class="item-address">
+              <p><a href="#" target="_blank">925 Magazine Street</a></p>
+              <p>French Quarter</p>
+            </div>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
+          </div> 
+          <div class="item">
+            <h4><a href="http://www.patobriens.com/patobriens/default.asp" target="_blank">Pat O'Brien's</a></h4>
+            <div class="item-address">
+              <p><a href="#" target="_blank">718 St Peter Street</a></p>
+              <p>French Quarter</p>
+            </div>
+            <div class="item-text">
+              <p>###</p>
+            </div>
+            <p>###</p>
+            <p>###</p>
+            <p>###</p>
+          </div> 
         </div>
         <div class="carousel-controls">  
           <!-- Left and right controls -->
@@ -217,6 +362,7 @@
 
   <style type="text/css">
       .todo-detail { display: none; }
+      .todo-nav-close { display: none; }
   </style>
 
 <script type="text/javascript">
@@ -226,6 +372,13 @@
          $(".todo-detail").hide();
          $(".todo-landing").hide();
          $("#"+test).show();
+
+         if (test != "landing") {
+          $(".todo-nav-close").show();
+         } else {
+          $(".todo-nav-close").hide();
+         }
+            
     } );
   });
 </script>
